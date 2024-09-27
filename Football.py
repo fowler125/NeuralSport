358386e93e09f8747c38da1c08468abd702cce20
jabar <jabari_mf@hotmail.com>
2024-09-27 13:42:16 -0500

    added gitignore file and football.py changes

--- Football.py
+++ Football.py
@@ -1,5 +1,33 @@
+import logging
 import nfl_data_py as nfl
+import numpy as np
+import pandas as pd
 
-print(nfl.import_pbp_data([2024]))
-print(nfl.import_seasonal_pfr('pass', [2024]))
-print(nfl.import_weekly_pfr('pass', [2024]))
+
+
+
+def weeklyPfr(stat_type:str,years:list):
+    '''
+    Retrieve weekly PFR data for a specific statistic type and years.
+
+    Parameters:
+    - stat_type (str): The type of statistic to retrieve.
+    - years (list): A list of years to retrieve the data for.
+
+    Returns:
+    None
+
+    Saves the retrieved data to a CSV file in the 'data/football' directory with a filename based on the statistic type and years.
+    '''
+    logging.info(f"Retrieving weekly PFR data for {stat_type} and years: {years}")
+    data = nfl.import_weekly_pfr(stat_type, years)
+    data.to_csv(f"data/football/{stat_type}_{years}.csv")
+    return data
+
+def main():
+    #print(nfl.import_pbp_data([2024]))
+    #print(nfl.import_seasonal_pfr('pass', [2024]))
+    weeklyPfr('pass',[2024])
+
+if __name__ == "__main__":
+    main()
