e858b2c2d9579856cf524122279092ec64d02a8e
jabar <jabari_mf@hotmail.com>
2024-09-27 13:50:48 -0500

    weekly stats is correct folder for usage

--- Football.py
+++ Football.py
@@ -19,15 +19,28 @@ def weeklyPfr(stat_type:str,years:list):
 
     Saves the retrieved data to a CSV file in the 'data/football' directory with a filename based on the statistic type and years.
     '''
-    logging.info(f"Retrieving weekly PFR data for {stat_type} and years: {years}")
+    #logging.info(f"Retrieving weekly PFR data for {stat_type} and years: {years}")
     data = nfl.import_weekly_pfr(stat_type, years)
     data.to_csv(f"data/football/{stat_type}_{years}.csv")
+    return data
+
+def weeklyStats(years:list):
+   
+    #logging.info(f"Retrieving weekly stats data for years: {years}")
+    data = nfl.import_weekly_data(years)
+    data.to_csv(f"data/football/stats_{years}.csv")
+    return data
+def NGSdata(st_tpe:str, years:list):
+    data = nfl.import_ngs_data(stat_type = st_tpe,years=years)
+    data.to_csv(f"data/football/{st_tpe}_NGS{years}.csv")
     return data
 
 def main():
     #print(nfl.import_pbp_data([2024]))
     #print(nfl.import_seasonal_pfr('pass', [2024]))
-    weeklyPfr('pass',[2024])
+    print(nfl.see_weekly_cols())
+    print(weeklyStats([2024]))
+    #NGSdata('passing',[2024])
 
 if __name__ == "__main__":
     main()
