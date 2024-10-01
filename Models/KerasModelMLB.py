7022b4186ce3a6ccac9392bd31a18b837937c466
fowler125 <jabari_mf@hotmail.com>
2024-10-01 10:00:08 -0500

    changes for main, will start new function with proper-er code today

--- Models/KerasModelMLB.py
+++ Models/KerasModelMLB.py
@@ -15,6 +15,10 @@ class KerasPitcherModel:
         -DO NOT HARDCODE ANY VALUES, previous iteration had hardcoded python splits [:xxx], this is not dynamic, and isnt something we want, so from now 
         on we will use the split method off the keras documentation:
         https://keras.io/api/models/model_training_apis/
+
+        Different, should use early stopping, nth number of epochs
+
+        Hyperparamater Tuning as well needs to be implemented 
         """
         pitcher_df = pd.read_csv(f"data/clean/{self.id}.csv")
         pitcher_df_X = pitcher_df[["pitch_type","release_speed","release_pos_x","release_pos_z","spin_dir","spin_rate_deprecated","break_angle_deprecated","break_length_deprecated","stand","p_throws","type","balls","strikes","pfx_x","pfx_z","plate_x","plate_z","on_3b","on_2b","on_1b","outs_when_up","inning","vx0","vy0","vz0","ax","ay","az","sz_top","sz_bot","release_spin_rate","release_extension","release_pos_y","at_bat_number","pitch_number","pitch_name","spin_axis"]]
