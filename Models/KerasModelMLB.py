7fa1b0c79cc7bfd7a9ed739943c3328b89fd6a83
fowler125 <jabari_mf@hotmail.com>
2024-10-31 10:41:12 -0500

    Removed deprecated functions

--- Models/KerasModelMLB.py
+++ Models/KerasModelMLB.py
@@ -18,81 +18,18 @@ features_dict = {
             "sz_bot","release_spin_rate","release_extension","release_pos_y","at_bat_number",
             "pitch_number","pitch_name","spin_axis"],
     "velocity":["vx0","vy0","vz0"],
-    "numerical_only":["vx0","vy0","vz0","ax","ay","az","pfx_x","pfx_z","plate_x","plate_z"],
+    "numerical_only":["vx0","vy0","vz0","ax","ay","az","plate_x","plate_z"],
     "plotting_data":["vx0","vy0","vz0","ax","ay","az","pfx_x","pfx_z","plate_x","plate_z","sz_top","sz_bot"]
 }
 class KerasPitcherModel:
     def __init__(self,id) -> None:
         self.id = id
-    def deprecated_plotting(self):
-        """ for loop to go thru all the features, and plot them in a graph, x axis = features, y axis= zone"""
-        pitcher_df = pd.read_csv(f"data/clean/{self.id}.csv")
-        pitcher_df_X = pitcher_df[["pitch_type","release_speed","release_pos_x","release_pos_z","spin_dir","spin_rate_deprecated","break_angle_deprecated","break_length_deprecated","stand","p_throws","type","balls","strikes","pfx_x","pfx_z","plate_x","plate_z","on_3b","on_2b","on_1b","outs_when_up","inning","vx0","vy0","vz0","ax","ay","az","sz_top","sz_bot","release_spin_rate","release_extension","release_pos_y","at_bat_number","pitch_number","pitch_name","spin_axis"]]
-        pitcher_df_Y = pitcher_df[["zone"]]
-
-        for i in pitcher_df.columns:
-            plt.scatter(pitcher_df[i],pitcher_df_Y)
-            plt.show()
 
     def correlation_matrix(self,data:pd.DataFrame):
         corr_matrix = data.corr()
         plt.figure(figsize=(10,8))
         sns.heatmap(corr_matrix, annot=True,cmap='coolwarm',square=True)
         plt.title("Correlation Matrix")
-        plt.show()
-    def deprecated_pitch_plotting(self,data:pd.DataFrame): 
-        
-        
-        # Create a new figure
-        plt.figure(figsize=(8, 8))
-        
-        # Get the maximum and minimum x and z values from the data
-        x_max = data['plate_x'].max()
-        x_min = data['plate_x'].min()
-        z_max = data['plate_z'].max()
-        z_min = data['plate_z'].min()
-        
-        # Calculate the range of the x and z values
-        x_range = x_max - x_min
-        z_range = z_max - z_min
-        
-        # Calculate the maximum range of the x and z values
-        max_range = max(x_range, z_range)
-        
-        # Calculate the scaling factor for the x and z values
-        x_scale = (400 / max_range)
-        z_scale = (400 / max_range)
-        
-        # Define a dictionary to map zone numbers to colors
-        zone_colors = {
-            1: 'red',
-            2: 'orange',
-            3: 'yellow',
-            4: 'green',
-            5: 'blue',
-            6: 'purple',
-            7: 'pink',
-            8: 'brown',
-            9: 'gray',
-            10: 'black',
-            11: 'white'
-        }
-        
-        # Plot each pitch using scaled plate_x and plate_z coordinates and zone-based colors
-        for index, row in data.iterrows():
-            zone = row['zone']
-            color = zone_colors.get(zone, 'black')
-            plt.scatter((row['plate_x'] - (x_max + x_min) / 2) * x_scale, (row['plate_z'] - (z_max + z_min) / 2) * z_scale, c=color, s=50, alpha=0.5)
-        
-        # Add a line at the origin (middle of the plot)
-        plt.axvline(0, color='k', linestyle='--', alpha=0.5)
-        plt.axhline(0, color='k', linestyle='--', alpha=0.5)
-        
-        # Set the limits of the plot to be symmetric around the origin
-        plt.xlim(-400, 400)
-        plt.ylim(-400, 400)
-        
-        # Show the plot
         plt.show()
         
     def pitcher_plotting(self,data:pd.DataFrame):
@@ -149,7 +86,7 @@ class KerasPitcherModel:
         pitcher_df = pd.read_csv(f"data/clean/{self.id}.csv")
         pitcher_unclean = pd.read_csv(f"data/unclean/{self.id}.csv")
         
-        pitcher_df_X = pitcher_df[features_dict["plotting_data"]]
+        pitcher_df_X = pitcher_df[features_dict["numerical_only"]]
         pitcher_df_X = pitcher_df_X.dropna(how="any")
         
         pitcher_df_Y = pitcher_df[["zone"]]
@@ -158,9 +95,9 @@ class KerasPitcherModel:
 
         print(pitcher_df_X.shape)
         print(pitcher_df_Y.shape)
-        #print(self.correlation_matrix(pitcher_df_X))
+        print(self.correlation_matrix(pitcher_df_X))
 
-        #print(self.pitcher_plotting(pitcher_unclean))
+        print(self.pitcher_plotting(pitcher_unclean))
         
         le = LabelEncoder()
         pitcher_df_Y = le.fit_transform(pitcher_df_Y)
@@ -272,37 +209,6 @@ class KerasPitcherModel:
 
         
 
-    def deprecated_setup_pitcher_df(self):
-        """
-        :param self (int) players id value, important for identifying player and csv location
-        """
-        pitcher_df = pd.read_csv(f"data/clean/{self.id}.csv")
-        reduced_df_X = pitcher_df[["vx0","vy0","vz0"]]
-        reduced_df_Y = pitcher_df[["zone"]]
-
-        X_train, X_valid, X_test = reduced_df_X[:31668], reduced_df_X[31668:38454], reduced_df_X[38454:]
-        y_train, y_valid, y_test = reduced_df_Y[:31668], reduced_df_Y[31668:38454], reduced_df_Y[38454:]
-
-        test = keras.Input(shape = (3,))
-
-        dense = layers.Dense(64, activation="relu")
-        x = dense(test)
-
-        StrikeZone = layers.Dense(13)(x)
-
-        model = keras.Model(inputs=test, outputs=StrikeZone, name="mnist_model")
-
-        model.compile(
-            loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-            optimizer=keras.optimizers.RMSprop(),
-            metrics=["accuracy"],
-        )
-
-        history = model.fit(X_train, y_train, epochs=100)
-
-        test_scores = model.evaluate(X_test, y_test, verbose=2)
-        print("Test loss:", test_scores[0])
-        print("Test accuracy:", test_scores[1])
 
 
 
